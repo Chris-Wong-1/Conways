5d17bbded3b6af963df77de2973c6a307e4e9a36
chris-wong-1 <wong.christopher.s@gmail.com>
2016-07-23 13:02:13 -0700

    Create base component structure for launch scene

---
+++ src/components/scenes/LaunchScene.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {
+  Scene
+} from 'react-native';
+
+class LaunchScene extends Component {
+  render() {
+    return (
+    	<Scene/>
+    );
+  }
+}
+
+module.exports = LaunchScene;
