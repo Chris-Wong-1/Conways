670ca966a951f9ee7462ccc86d147ff755123a8b
Jonathan Hall <ohsnapjon@gmail.com>
2016-07-24 21:10:45 -0700

    Update <About/> with basic game description and logo

--- src/components/About.js
+++ src/components/About.js
@@ -7,8 +7,8 @@ import {
 class About extends Component {
   render() {
     return (
-      <Text style={{fontSize: 20, fontWeight: 'bold'}}>
-        Hello Conways
+      <Text style={{fontSize: 18}}>
+        Conways is inspired by John Horton Conway's cellular automaton; the "Game of Life". As an artistic retelling of the fabled zero-player game, Conways combines music, art, and modern technological poetry to simulate Life based on four simple rules.
       </Text>
     );
   }
