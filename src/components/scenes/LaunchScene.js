7aab0f92a22f32f561468e334e83f323f47c182c
Macbook Air <macbookair@Macbooks-MacBook-Air.local>
2016-07-26 16:45:36 -0700

    Updated this page to render the play menu and avout button

--- src/components/scenes/LaunchScene.js
+++ src/components/scenes/LaunchScene.js
@@ -4,21 +4,24 @@ import {
   Navigator,
   TouchableHighlight,
   ListView,
-  StyleSheet
+  StyleSheet,
+  Text,
 
 } from 'react-native';
 
 import Title from '../Title'
 import StartButton from '../StartButton'
 import MenuButton from '../MenuButton'
+import AboutButton from '../AboutButton'
 
 class LaunchScene extends Component {
   render() {
     return (
     	<View style={styles.home}>
-    
         <View>
           <StartButton passingNavigator={this.props.navigator}/>
+          <MenuButton passingNavigator={this.props.navigator}/>
+          <AboutButton passingNavigator={this.props.navigator}/>
         </View>
       </View>
     );
@@ -28,8 +31,9 @@ class LaunchScene extends Component {
 const styles = StyleSheet.create({
 home:{
   flexDirection: "column",
-
-  flex: 1
+  flex: 1,
+  justifyContent: 'center',
+  alignItems: 'center',
 }
 });
 
