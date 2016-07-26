4dcf3b1e7567e39f6a7e4e6eb92863917c2af3f8
Macbook Air <macbookair@Macbooks-MacBook-Air.local>
2016-07-26 09:53:55 -0700

    Create routes and add navigator to root controller

--- src/RootController.js
+++ src/RootController.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
 import {
-  AppRegistry
+  AppRegistry,
+  Navigator
 } from 'react-native';
 
-import GameScene from './components/scenes/GameScene'
+import LaunchScene from './components/scenes/GameScene'
+
 
 class RootController extends Component {
-  render() {
-    return (
-    	<GameScene/>
-    );
-  }
+  renderScene(route, navigator) {
+ if(route.name == 'LaunchScene') {
+   return <LaunchScene navigator={navigator} />
+ }
+ if(route.name == 'GameScene') {
+   return <GameScene navigator={navigator} />
+ }
+ if(route.name == 'MenuScene') {
+   return <MenuScene navigator={navigator} />
+ }
+}
+
+
+render() {
+  return (
+    <Navigator
+  style={{ flex:1 }}
+  initialRoute={{ name: 'LaunchScene' }}
+  renderScene={ this.renderScene } />
+  )
+}
 }
 
 module.exports = RootController;
