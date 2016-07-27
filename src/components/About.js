7b6c4074d63ecde2ffc621936b42c37fbbf76827
Macbook Air <macbookair@Macbooks-MacBook-Air.local>
2016-07-26 20:43:24 -0700

    add basic css to style the about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
 import {
   // Any ReactNative Component Modules go here
-  Text
+  Text,TouchableHighlight,View,StyleSheet,
 } from 'react-native';
 
 class About extends Component {
+  _navigate(){
+    this.props.navigator.pop({
+
+    })
+  }
   render() {
     return (
-      <Text style={{fontSize: 18}}>
+      <View>
+      <TouchableHighlight
+        style={styles.Menu}
+        onPress={ () => this._navigate()}>
+        <Text style={styles.back}>back</Text>
+      </TouchableHighlight>
+      <Text style={styles.about}>
         Conways is inspired by John Horton Conway's cellular automaton; the "Game of Life". As an artistic retelling of the fabled zero-player game, Conways combines music, art, and modern technological poetry to simulate Life based on four simple rules.
       </Text>
+      </View>
     );
   }
 }
+const styles = StyleSheet.create({
+  about:{
+    fontSize: 16,
+    paddingTop: 15,
+    marginTop: 60
+  },
+  back:{
+    marginTop:15
+  }
+
+});
+
 
 // Description/Team Links Etc Go Here (static?)
 
